d86a3b7af4725b6f8a4feb4c0fe5965e4cde1ce5
Ammar <Ammardayoub6@gmail.com>
2019-02-07 20:27:19 +1100

    Added bot file
    Added main bot application
    Added NPM package lock file
    Added .gitignore rule to ignore secrets file with bot token

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const Secrets = require('secrets.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+
+client.login(Secrets.bot_secret_token)
