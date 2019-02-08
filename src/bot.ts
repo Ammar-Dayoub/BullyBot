9f418cfe3064197f4b1a53958b3dd2feb3020320
Ammar Dayoub <ammardayoub6@gmail.com>
2019-02-08 21:58:31 +1100

    The bot now uses discord commando libraries to parse commands, Moved the commands to a separate directory, added typescript linter

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,22 @@
-import * as Discord from 'discord.js';
-import * as Secrets from './secrets.json';
-const client = new Discord.Client();
+import { CommandoClient } from "discord.js-commando";
+import { join } from "path";
+import Secrets from "./secrets.json";
 
-client.on('ready', () => {
+const client = new CommandoClient({
+    commandPrefix: Secrets.command_prefix,
+    owner: Secrets.owner_id,
+});
+
+//  Register commands
+client.registry
+    // Registers custom groups
+    .registerGroup("admin", "Admin")
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(join(__dirname, "commands"));
+
+client.on("ready", () => {
     // List servers the bot is connected to
     console.log("Servers:");
     client.guilds.forEach((guild) => {
@@ -15,91 +29,12 @@ client.on('ready', () => {
         console.log();
     });
 
-    client.on('message', (receivedMessage) => {
+    client.on("message", (receivedMessage) => {
         // Prevent bot from responding to its own messages
-        if (receivedMessage.author == client.user) {
+        if (receivedMessage.author === client.user) {
             return;
         }
-
-        // Process command message
-        if (receivedMessage.content.startsWith("!")) {
-            processCommand(receivedMessage);
-            return;
-        }
-
-        // Normal bot functions
-        receivedMessage.channel.send("Message received: " + receivedMessage.content);
     });
 });
 
 client.login(Secrets.bot_secret_token);
-
-// Helper functions
-function processCommand(receivedMessage: Discord.Message) {
-    let fullCommand: string = receivedMessage.content.substr(1); // Remove the leading exclamation mark
-    let splitCommand = fullCommand.split(" "); // Split the message up in to pieces for each space
-    let primaryCommand = splitCommand[0]; // The first word directly after the exclamation is the command
-    let args = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
-
-    console.log("Command issues by: " + receivedMessage.author.username);
-    console.log("Command received: " + primaryCommand);
-    console.log("Arguments: " + args); // There may not be any arguments
-    console.log();
-
-    switch (primaryCommand) {
-        case "help":
-            helpCommand(args, receivedMessage);
-            break;
-        case "purge":
-            purgeCommand(args, receivedMessage);
-            break;
-        case "ping":
-            pingCommand(args, receivedMessage);
-            break;
-        default:
-            receivedMessage.channel.send("I don't understand the command.");
-            break;
-    }
-}
-function purgeCommand(args: string[], receivedMessage: Discord.Message) {
-    if (args.length > 0) {
-        switch (args[0]) {
-            case "channel":
-                if (receivedMessage.channel instanceof Discord.TextChannel) {
-                    receivedMessage.channel.fetchMessages().then(
-                        messages => messages.forEach(message => {
-                            message.deletable && message.delete();
-                        })
-                    );
-                } else {
-                    receivedMessage.channel.send("This Channel is not a text channel.");
-                }
-                break;
-            case "self":
-                if (receivedMessage.channel instanceof Discord.TextChannel) {
-                    receivedMessage.channel.fetchMessages().then(
-                        messages => messages.forEach(message => {
-                            message.deletable && message.author == client.user && message.delete();
-                        })
-                    );
-                } else {
-                    receivedMessage.channel.send("This Channel is not a text channel.");
-                }
-                break;
-            default:
-                receivedMessage.channel.send("I don't understand the command.");
-                break;
-        }
-    } else {
-        receivedMessage.channel.send("I don't understand the command.");
-    }
-}
-function pingCommand(args: string[], receivedMessage: Discord.Message) {
-    receivedMessage.channel.send(client.ping + "ms");
-}
-function helpCommand(args: string[], receivedMessage: Discord.Message) {
-    receivedMessage.channel.send('Available commands: \n' +
-                                 '!help\n' +
-                                 '!purge (channel, self)\n' +
-                                 '!ping\n');
-}
---
+++ src/commands/purge.ts
@@ -0,0 +1,46 @@
+import { Message, TextChannel } from "discord.js";
+import { Command, CommandMessage, CommandoClient } from "discord.js-commando";
+
+// tslint:disable-next-line:class-name
+export class purgeCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            description: "Deletes all messages",
+            examples: ["purge channel", "purge self"],
+            group: "admin",
+            memberName: "purge",
+            name: "purge",
+        });
+    }
+
+    public async run(recievedMessage: CommandMessage, args: object | string | string[]): Promise<Message | Message[]> {
+        if (recievedMessage.channel instanceof TextChannel) {
+            if (args) {
+                switch (args) {
+                    case "channel":
+                        recievedMessage.channel.fetchMessages().then(
+                            (messages) => messages.forEach((message) => {
+                                message.deletable && message.delete();
+                            }),
+                        );
+                        break;
+                    case "self":
+                        recievedMessage.channel.fetchMessages().then(
+                            (messages) => messages.forEach((message) => {
+                                message.deletable && message.author === this.client.user && message.delete();
+                            }),
+                        );
+                        break;
+                    default:
+                        recievedMessage.channel.send("I don't understand the command.");
+                        break;
+                }
+            } else {
+                recievedMessage.channel.send("I don't understand the command.");
+            }
+        } else {
+            recievedMessage.reply("This Channel is not a text channel.");
+        }
+        return;
+    }
+}
